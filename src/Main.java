1ce3e67020a347bc4b657d8bd32eb1d58216a81e
MariusMitrikevicius <zandenis@gmail.com>
2025-01-08 23:33:07 +0200

    + Berzas ir sukeitaliojau vietomis medzius.

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         // Sukuriame įvairių medžių sąrašus
-        List<Medis> medziai = List.of(new Azuolas(), new Berzas(), new Egle(), new Kadagys(), new Pusis());
-        List<Spygliuotis> spygliuociai = List.of(new Egle(), new Kadagys(), new Pusis());
-        List<Berzas> berzai = List.of(new Berzas(), new Berzas());
+        List<Medis> medziai = List.of(new Berzas(), new Azuolas(), new Kadagys(), new Egle(), new Pusis());
+        List<Spygliuotis> spygliuociai = List.of(new Kadagys(), new Egle(),  new Pusis());
+        List<Berzas> berzai = List.of(new Berzas(), new Berzas(), new Berzas());
 
         // Iškviečiame metodus su skirtingais sąrašais
         Miskas.ivairusMiskas(medziai);        // Priima bet kokį medžių sąrašą
